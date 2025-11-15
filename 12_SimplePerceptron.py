a4f8062d820ceca795cb6eeb65047ae20552ea04
Lakshay Saini <166095046+LakshaySaini06@users.noreply.github.com>
2025-11-15 19:27:46 +0530

    Add files via upload

---
+++ 12_SimplePerceptron.py
@@ -0,0 +1,12 @@
+def step_func(x):
+    return 1 if x>=0 else 0
+
+
+def perceptron(x1, x2, w1 , w2 , b):
+    z = (w1 * x1 + w2* x2 ) + b
+    return step_func(z)
+
+print(perceptron(0 , 0 , 1 , 1 ,-1.5))
+print(perceptron(1 , 0 , 1 , 1 ,-1.5))
+print(perceptron(0 , 1 , 1 , 1 ,-1.5))
+print(perceptron(1 , 1 , 1 , 1 ,-1.5))
